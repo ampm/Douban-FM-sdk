6c89f287782bce78e5f44e99afb2d9f29ac4efbd
ampm <hellooooy@gmail.com>
2014-06-01 01:19:52 +0800

    硬编码换做常量

---
+++ src/main/java/com/zzxhdzj/douban/ChannelConstantIds.java
@@ -0,0 +1,12 @@
+package com.zzxhdzj.douban;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yangning.roy@snda.com
+ * Date: 6/1/14
+ * To change this template use File | Settings | File Templates.
+ */
+public class ChannelConstantIds {
+    public static final int PRIVATE_CHANNEL = 0;
+    public static final int FAV = -3;
+}
