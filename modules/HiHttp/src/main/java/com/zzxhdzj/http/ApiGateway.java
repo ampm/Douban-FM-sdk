e7c1e0b03bd465554c1fdaa67d9c92f9840313cb
ampm <hellooooy@gmail.com>
2014-06-09 02:12:10 +0800

    #6

--- modules/HiHttp/src/main/java/com/zzxhdzj/http/ApiGateway.java
+++ modules/HiHttp/src/main/java/com/zzxhdzj/http/ApiGateway.java
@@ -1,6 +1,7 @@
 package com.zzxhdzj.http;
 
 import android.annotation.TargetApi;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.util.Log;
 import org.apache.http.client.methods.HttpGet;
@@ -8,8 +9,6 @@ import org.apache.http.client.methods.HttpPost;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -24,16 +23,12 @@ public class ApiGateway <T extends ApiResponse>{
     private static final int DEFAULT_MAX_CONNECTIONS = 5*CUP_CORE_NUM;
     private final Http http = new Http();
 
-    private ExecutorService threadPool;
-
-    public ApiGateway() {
-        threadPool = Executors.newFixedThreadPool(DEFAULT_MAX_CONNECTIONS);
-    }
 
     @TargetApi(Build.VERSION_CODES.CUPCAKE)
     public void makeRequest(ApiRequest<T> apiRequest, final ApiResponseCallbacks<T> responseCallbacks) {
         responseCallbacks.onStart();
-        new RemoteCallTask(responseCallbacks).execute(apiRequest);
+        //不会做
+        new RemoteCallTask(responseCallbacks).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,apiRequest);
     }
 
     protected void dispatch(T apiResponse, ApiResponseCallbacks<T> responseCallbacks) {
@@ -60,53 +55,53 @@ public class ApiGateway <T extends ApiResponse>{
     }
 
     @TargetApi(Build.VERSION_CODES.CUPCAKE)
-    private class RemoteCallTask{
+    private class RemoteCallTask extends AsyncTask<ApiRequest<T>, Void, T> {
         private final ApiResponseCallbacks<T> responseCallbacks;
 
         public RemoteCallTask(ApiResponseCallbacks<T> responseCallbacks) {
             this.responseCallbacks = responseCallbacks;
         }
 
-        public void execute(final ApiRequest<T> apiRequest) {
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-                    InputStream responseBody = null;
-                    Http.Response response;
-                    T apiResponse;
-                    try {
-                        try {
-                            Log.d("ApiGateway", "req info [url=" + apiRequest.getUrlString() + " ]");
-                            if (HttpPost.METHOD_NAME.equals(apiRequest.getMethod())) {
-                                response = http.post(apiRequest.getUrlString(), apiRequest.getHeaders(), apiRequest.getPostEntity(), apiRequest.allowRedirect);
-                            } else if (HttpGet.METHOD_NAME.equals(apiRequest.getMethod())) {
-                                response = http.get(apiRequest.getUrlString(), apiRequest.getHeaders(), apiRequest.allowRedirect);
-                            } else {
-                                throw new RuntimeException("Unsupported Http Method!");
-                            }
-                        } catch (Exception e) {
-                            Log.e("ApiGateway", "request failed\n" + e.getStackTrace().toString());
-                            apiResponse = apiRequest.createResponse(WrappedHttpError.REQUEST_ERROR.getCode(), null);
-                            dispatch(apiResponse, responseCallbacks);
-                            return;
-                        }
 
-                        try {
-                            responseBody = response.getResponseBody();
-                            apiResponse = apiRequest.createResponse(response.getStatusCode(), response.getHeaderFields());
-                            Log.d("ApiGateway", "resp info [response code = " + apiResponse.getHttpResponseCode() + "]\n");
-                            apiResponse.consumeResponse(responseBody);
-                        } catch (Exception e) {
-                            Log.d("ApiGateway", "resp process failed\n");
-                            apiResponse = apiRequest.createResponse(WrappedHttpError.CONSUME_ERROR.getCode(), (response == null) ? null : response.getHeaderFields());
-                        }
-                        dispatch(apiResponse, responseCallbacks);
-                    } finally {
-                        closeStream(responseBody);
+        @Override
+        protected T doInBackground(ApiRequest<T>... apiRequests) {
+            ApiRequest<T> apiRequest = apiRequests[0];
+            InputStream responseBody = null;
+            Http.Response response;
+            try {
+                try {
+                    Log.d("ApiGateway", "req info [url=" + apiRequest.getUrlString() + " ]");
+                    if (HttpPost.METHOD_NAME.equals(apiRequest.getMethod())) {
+                        response = http.post(apiRequest.getUrlString(), apiRequest.getHeaders(), apiRequest.getPostEntity(), apiRequest.allowRedirect);
+                    } else if (HttpGet.METHOD_NAME.equals(apiRequest.getMethod())) {
+                        response = http.get(apiRequest.getUrlString(), apiRequest.getHeaders(), apiRequest.allowRedirect);
+                    } else {
+                        throw new RuntimeException("Unsupported Http Method!");
                     }
+                } catch (Exception e) {
+                    Log.e("ApiGateway","request failed\n"+e.getStackTrace().toString());
+                    return apiRequest.createResponse(WrappedHttpError.REQUEST_ERROR.getCode(), null);
                 }
-            };
-            threadPool.submit(runnable);
+
+                try {
+                    responseBody = response.getResponseBody();
+                    T apiResponse = apiRequest.createResponse(response.getStatusCode(), response.getHeaderFields());
+                    Log.d("ApiGateway", "resp info [response code = " + apiResponse.getHttpResponseCode() + "]\n");
+                    apiResponse.consumeResponse(responseBody);
+                    return apiResponse;
+                } catch (Exception e) {
+                    Log.d("ApiGateway", "resp process failed\n");
+                    return apiRequest.createResponse(WrappedHttpError.CONSUME_ERROR.getCode(), (response == null) ? null : response.getHeaderFields());
+                }
+            }finally {
+                closeStream(responseBody);
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(T apiResponse) {
+            dispatch(apiResponse, responseCallbacks);
         }
     }
 }
